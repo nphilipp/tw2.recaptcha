a3e1ccdd4c914a32f04bd52f2e4226a7465579d1
Nils Philippsen <nils@redhat.com>
2014-03-25 15:01:21 +0100

    fix passing errors to recaptcha

--- tw2/recaptcha/widgets.py
+++ tw2/recaptcha/widgets.py
@@ -7,7 +7,7 @@ from tw2.core import templating, widgets
 class ReCaptchaWidget(InputField):
     template = """<div><script type="text/javascript" src="${w.server}/challenge?k=${w.public_key}${w.error_param}"></script>
 <noscript>
-  <iframe src="${w.server}/noscript?k=${w.public_key}${w.error_param}" height="300" width="500" frameborder="0"></iframe><br />
+  <iframe src="${w.server}/noscript?k=${w.public_key}${w.error_query_string}" height="300" width="500" frameborder="0"></iframe><br />
   <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
   <input type='hidden' name='recaptcha_response_field' value='manual_challenge' />
 </noscript></div>
@@ -21,13 +21,16 @@ class ReCaptchaWidget(InputField):
     error_param = Param(default=None, attribute=True)
     server = Param(default=API_SERVER, attribute=True)
 
+    error_query_string = Variable(default="")
+
     def prepare(self):
         """This method is called every time the widget is displayed. It's task
         is to prepare all variables that are sent to the template. Those
         variables can accessed as attributes of d."""
 
         if self.error_param:
-            self.error_param = '&error=%s'%error_param
+            self.safe_modify('error_query_string')
+            self.error_query_string = '&error=%s' % self.error_param
 
         if self.use_ssl:
             self.server = API_SSL_SERVER
